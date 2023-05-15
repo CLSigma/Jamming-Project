901e0d54a970a84f25a5ba5b79b7c3f4a9eb706d
CLSigma <korzenikn@gmail.com>
2023-05-15 19:17:03 +0200

    App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchBarContainer from './components/SearchBar/SearchBarContainer';
+import SearchResultsContainer from './components/SearchResults/SearchResultsContainer';
+import PlaylistContainer from './components/Playlist/PlaylistContainer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBarContainer/>
+      <div className="trackList">
+        <SearchResultsContainer/>
+        <PlaylistContainer/>
+      </div>
     </div>
   );
 }

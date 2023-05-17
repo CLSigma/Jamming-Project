ce415a0a8e2a2c165a3d484aa6e0b4709be0f3c1
CLSigma <korzenikn@gmail.com>
2023-05-17 14:04:54 +0200

    Add and remove from playlist

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import { useState } from 'react';
 import SearchBarContainer from './components/SearchBar/SearchBarContainer';
 import SearchResultsContainer from './components/SearchResults/SearchResultsContainer';
 import PlaylistContainer from './components/Playlist/PlaylistContainer';
+import Songs from './data/testResults';
 
 function App() {
+  const [Tracklist, setTracklist] = useState([]);
+  function addToPlaylist(id, title, author, album){
+    Tracklist.find(song => song[0] === id) === undefined ? 
+    setTracklist(Tracklist => [...Tracklist, [id, title, author, album]]) : console.log('Song already on tracklist');
+  }
+  function deleteFromPlaylist(id){
+    setTracklist(Tracklist => Tracklist.filter(song => song[0] !== id));
+  }
   return (
     <div className="App">
       <SearchBarContainer/>
       <div className="trackList">
-        <SearchResultsContainer/>
-        <PlaylistContainer/>
+        <SearchResultsContainer addToPlaylist={addToPlaylist} Songs={Songs}/>
+        <PlaylistContainer Tracklist={Tracklist} deleteFromPlaylist={deleteFromPlaylist}/>
       </div>
     </div>
   );
--- src/components/Playlist/PlaylistContainer.js
+++ src/components/Playlist/PlaylistContainer.js
@@ -1,11 +1,15 @@
 import "./PlaylistContainer.css";
 import TracklistContainer from "../Tracklist/TracklistContainer";
-function PlaylistContainer(){
+
+function PlaylistContainer(props){
+  function sendId(id){
+    props.deleteFromPlaylist(id);
+  }
     return (
       <div className="playlistContainer">
         <h2>New playlist</h2>
         <input type="text" className="nameInput"/>
-        <TracklistContainer />
+        <TracklistContainer Tracklist={props.Tracklist} sendId={sendId}/>
         <button>Save</button>
       </div>  
     );

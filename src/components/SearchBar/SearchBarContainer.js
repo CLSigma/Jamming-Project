1c27a30933b3d665a8a754971e86e254b5d4acd8
CLSigma <korzenikn@gmail.com>
2023-05-18 16:24:06 +0200

    Add axios, get songs from api and render data

--- src/components/SearchBar/SearchBarContainer.js
+++ src/components/SearchBar/SearchBarContainer.js
@@ -1,9 +1,16 @@
 import "./SearchBarContainer.css";
-function SearchBarContainer(){
+
+function SearchBarContainer(props){
     return (
       <div className="searchbar">
-        <input type="search" className="songInput"/>
-        <button>Search</button>
+        {props.token ? 
+        <form onSubmit={props.searchSongs}>
+          <input type="search" className="songInput" onChange={e => props.setSearchValue(e.target.value)}/>
+          <button type="submit">Search</button>
+        </form>
+        :
+        <h2>Please log in to search songs</h2>
+        }
       </div>  
     );
 }
